77471fe6d7056667304e1651dfdb1ee24a3a02c8
FAF <fridvinf@hiof.no>
2026-03-02 13:01:29 +0100

    Oppdatert API_KEY

--- Arbeidskrav 3/src/pages/Home.jsx
+++ Arbeidskrav 3/src/pages/Home.jsx
@@ -1,41 +1,42 @@
 import { useState } from "react"
 
-export default function Home() {
-
+export default function Home(){
     const [search, setSearch] = useState()
 
-    //ENDRE SENERE, IKKE LA API NØKKEl VÆRE HER
-    const baseUrl = `http://www.omdbapi.com/?apikey=` 
-    const apiKey = '4c1a31f' //FJERN NØKKEL SENERE
+    const baseUrl = `http://www.omdbapi.com/?s=${search}&apikey=`
+    const apiKey = import.meta.env.VITE_APP_API_KEY
 
-
-    const getMovies = async() =>{
-        try { 
-            const response = await fetch(`${baseUrl}${apiKey}&s=${search}`)
+    const getMovies = async()=>{
+        try
+        {
+            const response = await fetch(`${baseUrl}${apiKey}`)
             const data = await response.json()
-            console.log(data)
             
-        } catch (error) {
-            console.error(error);
+            console.log(data)
+
         }
-    } 
-    
-    const handleChange = (event) =>{
-        setSearch(event.target.value)
+        catch(err){
+            console.error(err);
+        }
     }
 
-    return( 
+    const handleChange = (e)=>{
+        setSearch(e.target.value)
+    }
+
+    return (
     <main>
         <h1>Forside</h1>
         <form>
             <label>
-                Search for a movie
-            <input type="search" placeholder="John Wick" onChange={handleChange}></input>
+                Søk etter film
+                <input type="search" placeholder="Harry Potter" onChange={handleChange}></input>
             </label>
-
         </form>
-        <button onClick={getMovies}>Search</button>    
+        <button onClick={getMovies}>Søk</button>
     </main>
-
+        
     )
+
+    
 }
