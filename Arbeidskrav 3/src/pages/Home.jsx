1a4e8940870887e7199396544acd015155652c2a
FAF <fridvinf@hiof.no>
2026-03-11 13:10:00 +0100

    Lagt til kravliste for etterkontroll

--- Arbeidskrav 3/src/pages/Home.jsx
+++ Arbeidskrav 3/src/pages/Home.jsx
@@ -1,4 +1,20 @@
 import Search from "../components/Search"
+// KRAV LISTE:
+// Inneholde søkefelt
+// Presentere opplising med filmer
+// Vise 10 James Bond filmer før søke
+
+// Søk skal gjennomføres etter 3 tegn 
+// Resultater skal baseres på tittel
+// Resultater skal erstatter default på hjemmeside
+// Hver filmvisning skal ha:
+//     - Tittel
+//     - Bilde 
+//     - Utgivelsesår
+
+// Rute for forside
+// Dynamisk rute for utvalgt film
+
 
 export default function Home(){
 
@@ -9,5 +25,4 @@ export default function Home(){
     </main>
         
     )
-
 }
