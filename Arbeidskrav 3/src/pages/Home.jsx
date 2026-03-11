cae37da14bbae9cb2b524f29a0658a88cd4bc118
FAF <fridvinf@hiof.no>
2026-03-11 10:21:57 +0100

    Search komponent
    Separerte Søk funksjon fra home

--- Arbeidskrav 3/src/pages/Home.jsx
+++ Arbeidskrav 3/src/pages/Home.jsx
@@ -1,66 +1,11 @@
-import { useState, useEffect } from "react"
-import MovieCard from "../components/MovieCard"
+import Search from "../components/Search"
 
 export default function Home(){
-    //Setter søket til "James Bond", bruker useEffect senere for å laste in filmene
-    const [search, setSearch] = useState("James Bond")
-    const [movies, setMovies] = useState([])
 
-
-
-    //Legger til '&type=movie' for å unngå serier og spill
-    const baseUrl = `http://www.omdbapi.com/?s=${search}&type=movie&apikey=`
-    const apiKey = import.meta.env.VITE_APP_API_KEY
-
-    const getMovies = async()=>{
-        try
-        {
-            const response = await fetch(`${baseUrl}${apiKey}`)
-            const data = await response.json()
-
-            if(data.Search) {
-                setMovies(data.Search)
-            }
-            
-            console.log("Data", data)
-
-        }
-        catch(err){
-            console.error(err);
-        }
-    }
-    
-    useEffect(() => {
-        getMovies()
-    }, [])
-
-    const handleChange = (e)=>{
-        setSearch(e.target.value)
-    }
-
-    const handleSubmit = (e) =>{
-        e.preventDefault()
-   
-    }
 
     return (
     <main id="home">
-        <article  className="search">
-            <h1>FILMSØK</h1>
-            <p>Søk etter film her:</p>
-            <form onSubmit={handleSubmit}>
-                <label >
-                    <input type="search" placeholder="James Bond" onChange={handleChange}></input>
-                </label>
-                <button onClick={getMovies}>Søk</button>    
-            </form>
-        </article>    
-        <section>
-            {movies.map((movie) => (
-            <MovieCard key={movie.imdbID + 'xt'} movie={movie} />   
-            ))}
-            
-        </section>
+        <Search />
     </main>
         
     )
